fa06885d4d0b48f40846273c0a1491b9db10522a
Caroline Genzel <carolinegenzel@MacBook-Pro-sl-Caroline.local>
2024-11-30 12:27:49 +0200

    done

--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        final int N=Integer.parseInt(args[0]);
+        boolean [] checkArr= new boolean[N+1];
+        for(int i=0;i<=N;i++){
+            if (i==0 || i==1) {
+                checkArr[i]=false;
+            }
+            else checkArr[i]=true;
+        }
+        int p=2;
+        int count=0;
+        while (p < Math.sqrt(N)) {
+            while (count<N) {
+                if (checkArr[count] && p!=count && count%p==0) {
+                    checkArr[count]=false;
+                }
+                count++;
+            }
+            p++;
+            count=p;
+        }
+    double numP=0.0;
+    for(int i=0;i<N;i++){
+        if (checkArr[i]) {
+            System.out.println(i);
+            numP++;
+
+        }
     }
-}
+    double odd= numP/N;
+    System.out.println("there are "+ (int)numP+ " primes between 2 and "+ N+" ("+(int)(odd*100)+"% are primes)");
+
+}}
