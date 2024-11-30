4563d92273f29f51cab998fe2521f75773717018
Caroline Genzel <carolinegenzel@Mac.lan>
2024-11-30 18:28:02 +0200

    done

--- Primes.java
+++ Primes.java
@@ -25,6 +25,8 @@ public class Primes {
     System.out.println(" ");
     System.out.println(" ");
     System.out.println(" ");
+    System.out.println(" ");
+
     for(int i=0;i<N;i++){
         if (checkArr[i]) {
             System.out.println(i);
